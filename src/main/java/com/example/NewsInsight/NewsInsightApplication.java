58a463560374f258372081a9a64c492ad0dd8ac5
JammySeok <jjh326326@naver.com>
2025-09-13 22:06:36 +0900

    Initial commit

---
+++ src/main/java/com/example/NewsInsight/NewsInsightApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewsInsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsInsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NewsInsight/NewsInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewsInsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
